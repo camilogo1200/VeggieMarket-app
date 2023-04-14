fee1b75bd939b29dccc9f8ef06c9e4dfb290f365
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2023-04-13 21:40:12 -0500

    Fixed nav

--- app/src/main/java/com/veggiemarket/ui/fragments/OnBoardingFragment.kt
+++ app/src/main/java/com/veggiemarket/ui/fragments/OnBoardingFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.veggiemarket.R
 import com.veggiemarket.databinding.OnBoardingFragmentBinding
 
@@ -27,6 +28,9 @@ class OnBoardingFragment : Fragment() {
     }
 
     private fun bindListeners() {
-
+        binding.nextBtn.setOnClickListener {
+            val navController =findNavController()
+            navController.navigate(R.id.action_onBoardingFragment_to_loginFragment)
+        }
     }
 }
